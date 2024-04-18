bb2e73aebbe24594ec7309b178a1acbd34fb1a28
Oleksandr Demchenko <olexandr.demchenko@gmail.com>
2024-04-18 17:54:40 +0300

    add and config gateway module, impl security on user module

---
+++ gateway-service/src/main/java/com/project/gatewayservice/config/GatewayConfig.java
@@ -0,0 +1,26 @@
+package com.project.gatewayservice.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class GatewayConfig {
+  @Bean
+  public RouteLocator routes(RouteLocatorBuilder builder) {
+    return builder.routes()
+        //USER-SERVICE ROUTING
+        .route(r -> r.path("/auth/register").filters(f -> f
+            .rewritePath("/auth/register", "/api/v1/users/register")
+        ).uri("lb://user-service"))
+        //AUTH-SERVICE ROUTING
+        .route(r -> r.path("/auth/login").filters(f -> f
+            .rewritePath("/auth/login", "/api/v1/auth/login")
+        ).uri("lb://auth-service"))
+        .build();
+  }
+}
---
+++ user-service/src/main/java/com/project/userservice/config/security/SecurityConfiguration.java
@@ -0,0 +1,57 @@
+package com.project.userservice.config.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Configuration class responsible for setting up security configurations.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+  private static final String[] AUTH_WHITELIST = {
+      // -- Auth
+      "/api/v1/auth/**",
+  };
+
+  private final JwtAuthConverter jwtAuthConverter;
+
+  /**
+   * Configures the security filter chain.
+   *
+   * @param http HttpSecurity object to configure security settings.
+   * @return SecurityFilterChain defining the security filter chain.
+   * @throws Exception if an error occurs while configuring security.
+   */
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .cors(withDefaults())
+        .authorizeHttpRequests(
+            authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers(AUTH_WHITELIST)
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .oauth2ResourceServer(
+            t -> t.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter)))
+        .sessionManagement(
+            httpSecuritySessionManagementConfigurer ->
+                httpSecuritySessionManagementConfigurer.sessionCreationPolicy(STATELESS));
+
+    return http.build();
+  }
+}
